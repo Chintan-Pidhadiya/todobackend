25d3ed99c88396e1e819ef5604f3dc1a716c9935
Chintan-Pidhadiya <ultracubes1@gmail.com>
2024-11-12 13:07:09 +0530

    minor change

--- route/user.route.js
+++ route/user.route.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { userController } = require('../controller');
+const express = require("express");
+const { userController } = require("../controller");
 
 const route = express.Router();
 
-route.post("/add", userController.addUser)
-route.get('/get', userController.getUser)
+route.post("/add", userController.addUser);
+route.get("/get", userController.getUser);
 
-module.exports = route
+module.exports = route;
